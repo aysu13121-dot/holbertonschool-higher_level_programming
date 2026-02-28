63cdf99ee9899a4c0b0515e5f38e6521672ba75a
aysu13121-dot <aysu13121@gamil.com>
2026-02-28 03:41:20 +0000

    Fix concat

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-number = 98
-language = "Python"
-print(word + ",")
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with clear syntax"
+str = str[39:66] + str[105:111] + str[0:6]
+print(str)
