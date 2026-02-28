ff0872c65b9d700b19f5df1591cc0f45d0d017be
aysu13121-dot <aysu13121@gamil.com>
2026-02-28 04:49:48 +0000

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = ("Python is an interpreted, interactive, object-oriented "
-        "programming  with Python, and beyond")
+       "programming  with Python, and beyond")
 str = str[39:66] + str[67:72] + str[72:79]
 print(str)
