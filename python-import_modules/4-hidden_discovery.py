87d2f56d52a0f02211348984151b30d7a17e872f
aysu13121-dot <aysu13121@gamil.com>
2026-03-11 19:37:24 +0000

    Add 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     def_names = dir(hidden_4)
 
     for i in range(len(def_names)):
-        if def_names[i][:2] != '_':
-	    print(def_names[i])
+        if def_names[i][:2] != '__':
+            print(def_names[i])
