55c23abe2bc7cb23b7c6bdc8ef0578ac499d72d2
aysu13121-dot <aysu13121@gmail.com>
2026-03-12 23:32:38 +0000

    add 5 no_c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
     result = []
     for char in my_string:
         if char != 'c' and char != 'C':
             result.append(char)
-
-
     return("".join(result))
