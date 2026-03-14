595285f4cbb7a6299497049359c800562104a214
aysu13121-dot <aysu1312@gamil.com>
2026-03-14 23:17:49 +0000

    add

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def multiple_returns(sentence:)
-    if len(sentece) > 0:
-        t = len(sentence), sentence[0]
-    else:
-        t = len(sentence), None
-    return (t)
+
+def multiple_returns(sentence):
+    return (len(sentence), None if len(sentence) == 0 else sentence[0])
+
