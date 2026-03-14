32776e0fb50c37b408a7330b25250317b64a7e25
aysu13121-dot <aysu1312@gamil.com>
2026-03-14 21:12:06 +0000

    add

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence:)
-
-    if len(sentence) == 0:
-        first_char = None
-    else:
-        first_char = sentence[0]
-
-    lenght = len(sentence)
-    tuple_result = (lenght, first_char)
-
-    return tuple_result
+    t = len(sentence), sentece[0]
+    return (t)
