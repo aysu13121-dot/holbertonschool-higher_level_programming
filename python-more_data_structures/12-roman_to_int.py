cba9b34a9e270f4b5d22639e16923e214a438e06
aysu13121-dot <aysu13121@gmail.com>
2026-03-21 10:22:32 +0000

    add

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D':500, 'M': 1000}
+    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     if type(roman_string) != str or roman_string is None:
         return result
+
     previous = None
     for letter in roman_string:
         n = numbers[letter]
+
         if previous is None:
             result = n
-            previuos = n
+            previous = n
             continue
         elif previous < n:
             result = result + n - previous * 2
