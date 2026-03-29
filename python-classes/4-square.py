46de052080a83e231b84248f390e47bd621cb235
aysu13121-dot <aysu13121@gmail.com>
2026-03-29 03:36:01 +0000

    add

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,7 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new Square."""
-        if not isintance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
@@ -21,7 +21,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isintance(value, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
