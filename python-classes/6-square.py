a81a55f87d429def6c101b9d5a32d8d762c54254
aysu13121-dot <aysu13121@gmail.com>
2026-04-04 11:36:09 +0000

    add

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,9 +37,8 @@ class Square:
                 not isinstance(value[1], int) or
                 value[0] < 0 or value[1] < 0):
             raise (TypeError("position must be a tuple of"
-                                         " 2 positive integers"))
+" 2 positive integers"))
         self.__position = value
-
     def area(self):
         """Return the area of the square."""
         return self.__size ** 2
