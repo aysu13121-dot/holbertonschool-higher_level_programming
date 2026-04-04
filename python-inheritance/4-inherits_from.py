f2e56757773ae0e516cedf2dcc9fb0b705c3d008
aysu13121-dot <aysu@gmail.com>
2026-04-04 16:15:40 +0000

    add

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines a inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """Return True if obj is instance of a class that inherited a_class"""
+    return isinstance(onj, a_class) and type(obj) is not a_class
