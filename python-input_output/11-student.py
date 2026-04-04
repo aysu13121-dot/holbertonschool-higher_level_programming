bfebe5901b7180382c243e49d469a367da4cb3a1
aysu13121-dot <aysu13121@gmail.com>
2026-04-04 21:51:41 +0000

    add

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a Student class"""
+
+
+class Student:
+    """A class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student"""
+        if isinstance(attrs, list):
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
