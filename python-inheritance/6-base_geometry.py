fd9431824b1a4d0685dd64718f5c76a075d85a7e
aysu13121-dot <aysu@gmail.com>
2026-04-04 16:25:17 +0000

    add

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,4 +6,4 @@ class BaseGeometry:
 
     def area(self):
         """Raises an Exception - area() is not implemented"""
-        raise Exception("area()-is not implemented")
+        raise Exception("area() is not implemented")
