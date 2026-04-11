bdeed245434b921e3daa028947bc868e84996010
aysu13121-dot <aysu13121@gmail.com>
2026-04-11 14:06:17 +0000

    Add

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -61,6 +61,8 @@ def add_user():
     username = user_data.get("username")
     if not username:
         return jsonify({"error": "Username is required"}), 400
+    if username in users:
+        return jsonify({{"error": "Username already exists"}), 409
     users[username] = user_data
     return jsonify({"message": "User added", "user": user_data}), 201
 
